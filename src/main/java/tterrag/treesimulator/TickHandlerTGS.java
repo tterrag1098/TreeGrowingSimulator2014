80b6bc0838aa8ecd051a6be03491b6bb6e49fca2
Spencer Alves <impiaaa@gmail.com>
2014-04-05 17:50:37 -0400

    Revert "Refactoring"
    This reverts commit 1f9e42aceb99a8b62ca2e4178819ab5f82adff7c.
    
    Conflicts:
    	src/main/java/tterrag/treesimulator/TickHandlerTGS.java
    	src/main/java/tterrag/treesimulator/TreeGrower.java

--- src/main/java/tterrag/treesimulator/TickHandlerTGS.java
+++ src/main/java/tterrag/treesimulator/TickHandlerTGS.java
@@ -9,7 +9,6 @@ import java.util.EnumSet;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -23,6 +22,7 @@ import static tterrag.treesimulator.TickHandlerTGS.PlayerState.*;
 public class TickHandlerTGS implements ITickHandler {
 
 	private double posX = 0, posZ = 0;
+	private int movementCounter = 0;
 	private int ticksSinceLastCheck = 0;
 
 	public enum PlayerState {
@@ -39,15 +39,41 @@ public class TickHandlerTGS implements ITickHandler {
 		if (type.contains(TickType.PLAYER))
 		{
 			EntityPlayer player = (EntityPlayer) tickData[0];
+			Player basePlayer = (Player) tickData[0];
 			if (ticksSinceLastCheck >= 5)
 			{
+				int[] pos = getNearestSapling(player.worldObj, (int) player.posX, (int) player.posY, (int) player.posZ);
+
+				if (pos.length == 0)
+					return;
+				
 				if ((Math.abs(player.posX - posX) > 0.25 || Math.abs(player.posZ - posZ) > 0.25) && TreeSimulator.sprintingWorks)
 				{
-					TreeGrower.instance.requestTreeGrowth((EntityPlayerMP)tickData[0]);
+					movementCounter++;
 				}
 				if ((PlayerState.getState(player.isSneaking()) != state) && TreeSimulator.crouchingWorks)
 				{					
-					TreeGrower.instance.requestTreeGrowth((EntityPlayerMP)tickData[0]);
+					movementCounter++;
+				}
+				if (movementCounter > TreeSimulator.waitTime)
+				{
+					if (pos.length == 0) 
+					{
+						movementCounter--;
+						updatePlayerPos(player);
+						return;
+					}
+
+					BonemealEvent event = new BonemealEvent(player, player.worldObj, player.worldObj.getBlockId(pos[0], pos[1], pos[2]), pos[0], pos[1], pos[2]);
+					MinecraftForge.EVENT_BUS.post(event);
+
+	        		if ((double)player.worldObj.rand.nextFloat() < 0.45D)
+	                	((BlockSapling)Block.blocksList[player.worldObj.getBlockId(pos[0], pos[1], pos[2])]).markOrGrowMarked(player.worldObj, pos[0], pos[1], pos[2], player.worldObj.rand);
+					
+					if (TreeSimulator.showParticles)
+						sendPacket(pos[0], pos[1], pos[2], player.worldObj, basePlayer);
+					
+					movementCounter = 0;
 				}
 			}
 			else
@@ -60,9 +86,43 @@ public class TickHandlerTGS implements ITickHandler {
 		}
 	}
 
+	private void sendPacket(int x, int y, int z, World worldObj, Player player) {
+
+		Packet250CustomPayload packet = new Packet250CustomPayload();
+
+		packet.channel = TreeSimulator.CHANNEL;
+
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream(12);
+		DataOutputStream stream = new DataOutputStream(byteStream);
+		try {
+			stream.writeInt(x);
+			stream.writeInt(y);
+			stream.writeInt(z);
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		packet.data = byteStream.toByteArray();
+		packet.length = byteStream.size();
+		PacketDispatcher.sendPacketToPlayer(packet, player);
+	}
+
 	private void updatePlayerPos(EntityPlayer player) {
 		posX = player.posX;
 		posZ = player.posZ;
+	}
+
+	private int[] getNearestSapling(World world, int xpos, int ypos, int zpos) {
+		for (int x = -5; x <= 5; x++)
+			for (int y = -2; y <= 2; y++)
+				for (int z = -5; z <= 5; z++) {
+					int id = world.getBlockId(x + xpos, y + ypos, z + zpos);
+					if (Block.blocksList[id] instanceof BlockSapling)
+						return new int[] { x + xpos, y + ypos, z + zpos };
+				}
+		return new int[] {};
 	}
 
 	@Override
