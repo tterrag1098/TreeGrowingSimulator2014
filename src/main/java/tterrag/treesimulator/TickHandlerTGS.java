2b45c28111bd78fddbeeed1a3db12eac2b59679c
tterrag1098 <tterrag1098@gmail.com>
2014-07-28 01:20:22 -0400

    Bug fixes:  - Players now have their own counters  - A random sapling in the vicinity is bonemealed each time  - Only one sapling per event

--- src/main/java/tterrag/treesimulator/TickHandlerTGS.java
+++ src/main/java/tterrag/treesimulator/TickHandlerTGS.java
@@ -2,8 +2,10 @@ package tterrag.treesimulator;
 
 import static tterrag.treesimulator.TickHandlerTGS.PlayerState.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSapling;
@@ -19,7 +21,7 @@ import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 public class TickHandlerTGS
 {
 	private double posX = 0, posZ = 0;
-	private int movementCounter = 0;
+	private Map<String, Integer> counters = new HashMap<String, Integer>();
 	private int ticksSinceLastCheck = 0;
 
 	public enum PlayerState
@@ -49,6 +51,8 @@ public class TickHandlerTGS
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event)
 	{
+	    Integer temp = counters.get(event.player.getCommandSenderName());
+	    int movementCounter = temp == null ? 0 : temp;
 	    System.out.println(movementCounter);
 	    
 		if (event.phase == Phase.END)
@@ -56,12 +60,12 @@ public class TickHandlerTGS
 			EntityPlayer player = (EntityPlayer) event.player;
 			if (ticksSinceLastCheck >= 5)
 			{
-				List<Coord> coords = getNearestBlocks(player.worldObj, (int) player.posX, (int) player.posY, (int) player.posZ);
+				Set<Coord> coords = getNearestBlocks(player.worldObj, (int) player.posX, (int) player.posY, (int) player.posZ);
 
 				if (coords.size() == 0)
 					return;
 
-				if (Math.abs(player.posX - posX) > 0.25 || Math.abs(player.posZ - posZ) > 0.25)
+				if (Math.abs(player.posX - posX) > 0.5 || Math.abs(player.posZ - posZ) > 0.5)
 				{
 					movementCounter++;
 					doEngines(coords, player.worldObj);
@@ -95,6 +99,8 @@ public class TickHandlerTGS
 
 							if (TreeSimulator.showParticles && sapling == Blocks.sapling)
 								sendPacket(pos.x, pos.y, pos.z);
+							
+							break;
 						}
 					}
 					movementCounter = 0;
@@ -107,10 +113,11 @@ public class TickHandlerTGS
 
 			state = PlayerState.getState(player.isSneaking());
 			updatePlayerPos(player);
+			counters.put(event.player.getCommandSenderName(), movementCounter);
 		}
 	}
 
-	private void doEngines(List<Coord> coords, World world)
+	private void doEngines(Set<Coord> coords, World world)
 	{
 		for (Coord pos : coords)
 		{
@@ -138,9 +145,9 @@ public class TickHandlerTGS
 		posZ = player.posZ;
 	}
 
-	private List<Coord> getNearestBlocks(World world, int xpos, int ypos, int zpos)
+	private Set<Coord> getNearestBlocks(World world, int xpos, int ypos, int zpos)
 	{
-		List<Coord> list = new ArrayList<Coord>();
+		Set<Coord> list = new HashSet<Coord>();
 		for (int x = -5; x <= 5; x++)
 			for (int y = -2; y <= 2; y++)
 				for (int z = -5; z <= 5; z++)
